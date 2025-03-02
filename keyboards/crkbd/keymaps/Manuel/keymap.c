6d3b13410a112e91d9073295b408247f01a12840
Manuel <me.lm2207@gmail.com>
2025-03-02 18:34:28 -0500

    fully configured keys and light

--- keyboards/crkbd/keymaps/Manuel/keymap.c
+++ keyboards/crkbd/keymaps/Manuel/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_CAPS,       KC_QUOT, KC_DQUO, KC_LBRC, KC_RBRC, XXXXXXX,                      XXXXXXX,  KC_DLR,  KC_GRV, KC_PIPE, KC_AMPR, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, RALT(KC_SLSH), XXXXXXX, KC_LCBR, KC_RCBR, XXXXXXX,                      XXXXXXX, XXXXXXX, KC_MINS, KC_PPLS, KC_UNDS, XXXXXXX,
+      KC_LSFT, RALT(KC_SLSH), XXXXXXX, KC_LCBR, KC_RCBR, XXXXXXX,                      XXXXXXX, XXXXXXX, KC_MINS, KC_PPLS, KC_UNDS, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LALT,   MO(4),  KC_SPC,     KC_SPC, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
